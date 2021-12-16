8d151f7a9dfbd51d623a1e6b40e7afd78bb00be1
Manoel Carvalho <manoel.carvalho@mediacaonline.com>
2021-12-16 15:13:16 -0300

    Change ScrollView to FlatList

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.8:3000/',
+  baseURL: 'https://d16b-2804-29b8-501a-12cb-41f9-1f32-eba8-89f4.ngrok.io',
 });
 
 export default api;
